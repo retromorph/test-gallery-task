009ac8890c3fac390f2f21e94970dcef330e6acf
retromorph <musalov2@gmail.com>
2021-09-10 04:26:56 +0300

    base structure

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'Navbar'
+})
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/pages/Masterpieces.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'Masterpieces'
+})
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
